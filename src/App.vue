2cbb36e999aabc597edbc3f092ed14f199974172
goeun <goeun@MacBook-Air.local>
2022-04-12 14:07:38 +0900

    lesson3

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,59 @@
 <template>
   <div class="app">
-    <p>{{ name }} - {{ age }}</p>
-    <button @click="changeName('zelda')">changeName</button>
-    <button @click="changeAge(30)">changeAge</button>
+    <p>{{ jobs[0].location  }}</p>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-
+import { defineComponent, reactive, ref, toRefs } from "vue";
+import Job from "./types/Job";
 export default defineComponent({
   name: "App",
   components: {},
-  data() {
-    return {
-      name: "Link",
-      age: 25 as number | string,
-    };
-  },
-  methods: {
-    changeName(name: string) {
-      this.name = name;
-      return name;
-    },
-    changeAge(age: number | string) {
-      this.age = age;
-      return age;
-    },
+  setup() {
+    // const state = reactive({
+    //   name: "Link",
+    //   age: 25 as string | number,
+    // });
+    // return { ...toRefs(state) };
+    // const name = ref("Link");
+    // const age = ref<number | string>(23);
+    // return { name, age };
+
+    const jobs = ref<Job[]>([
+      {
+        title: "farm worker",
+        location: "ranch",
+        salary: 30000,
+        id: "1",
+      },
+      {
+        title: "farm worker2",
+        location: "ranch",
+        salary: 30000,
+        id: "2",
+      },
+      {
+        title: "farm worker3",
+        location: "ranch",
+        salary: 30000,
+        id: "3",
+      },
+      {
+        title: "farm worker4",
+        location: "ranch",
+        salary: 30000,
+        id: "4",
+      },
+      {
+        title: "farm worker5",
+        location: "ranch",
+        salary: 30000,
+        id: "5",
+      },
+    ]);
+
+    return { jobs };
   },
 });
 </script>
